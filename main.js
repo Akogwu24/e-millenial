75cee707337cdf5df0f2733f632d9b88f2009256
Akogwu Emmanuel <akogwu.emmanuel24@gmail.com>
2021-04-09 15:50:22 +0100

    form validation bugs discovered

--- main.js
+++ main.js
@@ -291,17 +291,82 @@ function payWithPaystack() {
 }
 const checkOut = document.getElementById('checkout');
 checkOut.addEventListener('click', () => {
+  // checkFormInputs();
   payWithPaystack();
 });
+
+//modal form validation
+//getting the input elements
+// const userName = document.getElementById('user-name');
+// const userNameError = document.getElementById('user-name-error');
+// const email = document.getElementById('email');
+// const emailError = document.getElementById('email-error');
+// const phoneNumber = document.getElementById('phone-number');
+// const phoneNumberError = document.getElementById('phone-number-error');
+
+// function checkFormInputs() {
+//   const userNameValue = userName.value.trim();
+//   const emailValue = email.value.trim();
+//   const phoneNumberValue = phoneNumber.value.trim();
+//   console.log(userNameValue);
+//   //username validation
+//   userName.addEventListener('blur', () => {
+//     if (userNameValue == '') {
+//       userNameError.textContent = 'Name cannot be blank please input your name';
+//       userName.style.border = 'thin solid';
+//       userName.style.borderColor = 'red';
+//     } else {
+//       userName.style.border = 'thin solid';
+//       userName.style.borderColor = 'green';
+//     }
+//   });
+//   //email validation
+//   email.addEventListener('blur', () => {
+//     if (emailValue == '') {
+//       emailError.textContent =
+//         'email cannot be blank please input a valid email';
+//       email.style.border = 'thin solid';
+//       email.style.borderColor = 'red';
+//     } else {
+//       email.style.border = 'thin solid';
+//       email.style.borderColor = 'green';
+//     }
+//   });
+//   //phone number validation
+//   phoneNumber.addEventListener('blur', () => {
+//     if (phoneNumber == '' || phoneNumberValue.length < 11) {
+//       phoneNumberError.textContent = 'Enter a valid 11 digits phone number';
+//       phoneNumber.style.border = 'thin solid';
+//       phoneNumber.style.borderColor = 'red';
+//     } else {
+//       phoneNumber.style.border = 'thin solid';
+//       phoneNumber.style.borderColor = 'green';
+//     }
+//   });
+// }
+// checkFormInputs();
 
 //summary modal toogle
 function showSummary() {
   const summaryModal = document.querySelector('.summary-modal');
   summaryModal.classList.add('summary-modal-show');
+  let summaryModalContent = document.querySelector('.summary-modal-container');
+  const cartItems = localStorage.getItem('productsInCart');
+  cartItems = JSON.parse(cartItems);
+  let itemSummaryItems = document.querySelector('.item-summary-items');
+  if (cartItems) {
+    itemSummaryItems.innerHTML = '';
+    Object.values(cartItems).map((item) => {
+      itemSummaryItems.innerHTML += `
+      <span>${item.name}</span>
+      <span>${item.inCart}</span>
+      `;
+    });
+  }
   document.querySelector('body').style.overflow = 'hidden';
 }
 
-showSummary();
+// showSummary();
 
 //close summary modal
 const okBtn = document.getElementById('ok');
